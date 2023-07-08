f6f686a983bf56936c85b416e8c3f2fd561e7593
AnreX <han220173@gmail.com>
2023-07-08 08:06:46 +0700

    coach card 1

--- therapy_club/therapy/views.py
+++ therapy_club/therapy/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
-from therapy.models import Services
+from therapy.models import Services, Graphics, Coaches
 
 
 def index(request):
+    logo = Graphics.objects.get(title='логотип')
+    image = Coaches.objects.get(pk=1)
     context = {
         'title': 'Главная страница',
+        'logo': logo,
+        'coach_photo': image,
 
     }
 
